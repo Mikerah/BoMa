8a97ea588c88b4f212b444cf17867ab6e0e8693b
Mikerah <mikerah14@gmail.com>
2017-12-05 15:58:59 -0500

    Added general structure of app activities, added extra dependencies, started basic layout

---
+++ BoMa/app/src/main/java/com/example/mikerah/boma/BookManager.java
@@ -0,0 +1,8 @@
+package com.example.mikerah.boma;
+
+/**
+ * Created by Mikerah on 11/27/2017.
+ */
+
+public class BookManager {
+}
---
+++ BoMa/app/src/main/java/com/example/mikerah/boma/database/BookBaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.mikerah.boma.database;
+
+/**
+ * Created by Mikerah on 12/5/2017.
+ */
+
+public class BookBaseHelper {
+}
